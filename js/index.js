5919ac2570236c058bfae5d0e2f3cad8e1353437
Kris <krzysztofkoz6@gmail.com>
2021-11-20 00:38:01 +0100

    Add grid size slider

--- js/index.js
+++ js/index.js
@@ -1,13 +1,47 @@
-const sketchPad = document.querySelector(".sketch-container-js");
-const root = document.querySelector(":root");
+document.addEventListener("DOMContentLoaded", () => {
+	const defaultGridSize = input.value;
 
-function createInitialGrid(size) {
+	setDefaultSliderState();
+	createGrid(defaultGridSize);
+});
+
+const root = document.querySelector(":root");
+const sketchPad = document.querySelector(".sketch-container-js");
+
+const input = document.querySelector("input");
+const output = document.querySelector("output");
+
+input.addEventListener("input", handleInputChange);
+
+function setDefaultSliderState() {
+	output.value = input.value;
+}
+
+function createGrid(size) {
 	const sizeSquared = size * size;
 
 	for (let i = 0; i < sizeSquared; i++) {
+		console.log(sketchPad.childElementCount);
 		const divElement = document.createElement("div");
 		divElement.classList.add("grid-item");
 		sketchPad.appendChild(divElement);
 	}
 }
-createInitialGrid(16);
+
+function deleteGrid() {
+	while (sketchPad.hasChildNodes()) {
+		sketchPad.firstElementChild.remove();
+	}
+}
+
+function adjusGridSize(value) {
+	root.style.setProperty("--grid-size", value);
+}
+
+function handleInputChange({ target: { value } }) {
+	output.value = value;
+
+	deleteGrid();
+	createGrid(value);
+	adjusGridSize(value);
+}
